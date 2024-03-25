a34943210d849cc64fa267977739fdcaa66fce83
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-25 16:10:35 +0300

    working on methods

--- challenge.dart
+++ challenge.dart
@@ -3,10 +3,23 @@ class Student {
     int age,
     int grade level,
 
+void studentInfo() {
+    print("Name: $name");
+    print("Age: $age");
+    print("Grade level: $grade level");
+}
+
+
 }
 
 class Teacher {
     String name,
     int age,
     String subject,
+
+    void teacherInfo() {
+        print("Name: $name");
+        print("Age: $age");
+        print("Subject: $subject");
+    }
 }
