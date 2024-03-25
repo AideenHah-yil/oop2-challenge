bc27796c287c1e7ab9f1e7b32f3523bb9ec03a51
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-25 16:20:02 +0300

    debugging and finalizing code

--- challenge.dart
+++ challenge.dart
@@ -1,15 +1,13 @@
 class Student {
     String name,
     int age,
-    int grade level,
+    int gradeLevel,
 
 void studentInfo() {
     print("Name: $name");
     print("Age: $age");
-    print("Grade level: $grade level");
+    print("GradeLevel: $gradeLevel");
 }
-
-
 }
 
 class Teacher {
@@ -23,3 +21,27 @@ class Teacher {
         print("Subject: $subject");
     }
 }
+
+class infoPrinter {
+    void printInfo() {
+        var student = Student();
+        ..name = "Inosuke Hashibira"
+        ..age = 16
+        ..gradeLevel = 10;
+
+        var teacher = Teacher();
+        ..name = "Tengen Uzui"
+        ..age = 28
+        ..subject = "English";
+
+        print("Student Information:");
+        student.studentInfo();
+        print("\nTeacher Information:");
+        teacher.teacherInfo();
+    }
+    }
+
+    void main() {
+        var infoPrinter = infoPrinter();
+        infoPrinter.printInfo();
+    }
