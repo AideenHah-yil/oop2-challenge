03fb13e70a7bb8937ba62b8e13cb1149a222c6e8
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-25 16:35:41 +0300

    debugging

--- challenge.dart
+++ challenge.dart
@@ -1,52 +1,53 @@
 class Student {
-    String name = "";
-    int age = 0;
-    int gradeLevel = 0;
+  String name = "";
+  int age = 0;
+  int gradeLevel = 0;
 
-void studentInfo() {
+  void studentInfo() {
     print("Name: $name");
     print("Age: $age");
     print("GradeLevel: $gradeLevel");
-}
+  }
 }
 
 class Teacher {
-    String name = "";
-    int age = 0;
-    String subject = "";
+  String name = "";
+  int age = 0;
+  String subject = "";
 
-    void teacherInfo() {
-        print("Name: $name");
-        print("Age: $age");
-        print("Subject: $subject");
-    }
+  void teacherInfo() {
+    print("Name: $name");
+    print("Age: $age");
+    print("Subject: $subject");
+  }
 }
 
 class InfoPrinter {
-    factory InfoPrinter.create() {
-        return InfoPrinter._();
-    }
-infoPrinter._();
+  factory InfoPrinter.create() {
+    return InfoPrinter._();
+  }
 
-    void printInfo() {
-        var student = Student();
-        ..name = "Inosuke Hashibira"
-        ..age = 16
-        ..gradeLevel = 10;
+  InfoPrinter._(); // Private constructor
 
-        var teacher = Teacher();
-        ..name = "Tengen Uzui"
-        ..age = 28
-        ..subject = "English";
+  void printInfo() {
+    var student = Student()
+      ..name = "Inosuke Hashibira"
+      ..age = 16
+      ..gradeLevel = 10;
 
-        print("Student Information:");
-        student.studentInfo();
-        print("\nTeacher Information:");
-        teacher.teacherInfo();
-    }
-    }
+    var teacher = Teacher()
+      ..name = "Tengen Uzui"
+      ..age = 28
+      ..subject = "English";
 
-    void main() {
-        var InfoPrinter = InfoPrinter.create();
-        infoPrinter.printInfo();
-    }
+    print("Student Information:");
+    student.studentInfo();
+    print("\nTeacher Information:");
+    teacher.teacherInfo();
+  }
+}
+
+void main() {
+  var infoPrinter = InfoPrinter.create();
+  infoPrinter.printInfo();
+}
